1474e1827030c414d1868e3beaf6ad0682d37f0a
vagrant <vagrant@precise32.(none)>
2015-11-30 20:40:03 +0000

    Heroku init

--- controllers/user.js
+++ controllers/user.js
@@ -281,10 +281,10 @@ exports.postReset = function(req, res, next) {
     },
     function(user, done) {
       var transporter = nodemailer.createTransport({
-        service: 'SendGrid',
+        service: 'Mandrill',
         auth: {
-          user: secrets.sendgrid.user,
-          pass: secrets.sendgrid.password
+          user: secrets.mandrill.user,
+          pass: secrets.mandrill.password
         }
       });
       var mailOptions = {
@@ -356,10 +356,10 @@ exports.postForgot = function(req, res, next) {
     },
     function(token, user, done) {
       var transporter = nodemailer.createTransport({
-        service: 'SendGrid',
+        service: 'Mandrill',
         auth: {
-          user: secrets.sendgrid.user,
-          pass: secrets.sendgrid.password
+          user: secrets.mandrill.user,
+          pass: secrets.mandrill.password
         }
       });
       var mailOptions = {
