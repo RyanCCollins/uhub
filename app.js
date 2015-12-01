421e65d96b709d0f7178fd6a88339dc68e03e8fd
Ryan <techrapportrc@gmail.com>
2015-12-01 00:45:50 +0000

    Commit prior to keystone

--- app.js
+++ app.js
@@ -103,8 +103,8 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
 /**
  * Primary app routes.
  */
-app.get('/', homeController.admin);
-// app.get('/', homeController.index);
+// app.get('/', homeController.admin);
+app.get('/', homeController.index);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
